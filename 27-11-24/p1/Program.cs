31ba4d439fdb54db19d5157adc57e620a413d3a7
SumedhNaidu <sumedhnaidu.kn@gmail.com>
2024-11-27 18:17:10 +0530

    27-11-24

---
+++ 27-11-24/p1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SemaphoreSlimPratice
+{
+    class Program
+    {
+        //static SemaphoreSlim slim = new SemaphoreSlim(initialCount: 3);
+        //static void Main(string[] args)
+        //{
+        //    for (int i = 1; i <= 5; i++)
+        //    {
+        //        int count = i;
+        //        Thread t = new Thread(() => SemaphoreSlimFunction("Thread " + count, 1000 * count));
+        //        t.Start();
+        //    }
+        //    Console.ReadLine();
+        //}
+        //static void SemaphoreSlimFunction(string name, int seconds)
+        //{
+        //    Console.WriteLine($"{name} Waits to access resource");
+        //    slim.Wait();
+        //    Console.WriteLine($"{name} was granted access to resource");
+        //    Thread.Sleep(seconds);
+        //    Console.WriteLine($"{name} is completed");
+        //    slim.Release();
+        //}
+
+        // Create the semaphore.
+        private static SemaphoreSlim semaphore = new SemaphoreSlim(0, 3);
+
+        // A padding interval to make the output more orderly.
+        private static int padding;
+
+        public static void Main()
+        {
+            Console.WriteLine($"{semaphore.CurrentCount} tasks can enter the semaphore");
+            Task[] tasks = new Task[5];
+
+            // Create and start five numbered tasks.
+            for (int i = 0; i <= 4; i++)
+            {
+                tasks[i] = Task.Run(() =>
+                {
+                    // Each task begins by requesting the semaphore.
+                    Console.WriteLine($"Task {Task.CurrentId} begins and waits for the semaphore");
+
+                    int semaphoreCount;
+                    semaphore.Wait();
+                    try
+                    {
+                        Interlocked.Add(ref padding, 100);
+                        Console.WriteLine($"Task {Task.CurrentId} enters the semaphore");
+                        // The task just sleeps for 1+ seconds.
+                        Thread.Sleep(1000 + padding);
+                    }
+                    finally
+                    {
+                        semaphoreCount = semaphore.Release();
+                    }
+                    Console.WriteLine($"Task {Task.CurrentId} releases the semaphore; previous count: {semaphoreCount}");
+                });
+            }
+
+            // Wait for one second, to allow all the tasks to start and block.
+            Thread.Sleep(1000);
+
+            // Restore the semaphore count to its maximum value.
+            Console.Write("Main thread calls Release(3) --> ");
+            semaphore.Release(3);
+            Console.WriteLine($"{semaphore.CurrentCount} tasks can enter the semaphore");
+            // Main thread waits for the tasks to complete.
+            Task.WaitAll(tasks);
+
+            Console.WriteLine("Main thread Exits");
+            Console.ReadKey();
+        }
+    }
+}
+
+
---
+++ 27-11-24/p2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AsyncAwait
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main method Started");
+            SomeMethod();
+            Console.WriteLine("Main method End");
+            Console.ReadKey();
+        }
+        public async static void SomeMethod()
+        {
+            Console.WriteLine("Some Method Started");
+            //Thread.Sleep(TimeSpan.FromSeconds(10));
+            //await Task.Delay(TimeSpan.FromSeconds(10));
+            //Console.WriteLine("\n");
+            await Wait();
+            Console.WriteLine("Some Method Ended");
+        }
+        public static async Task Wait()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(10));
+            Console.WriteLine("\n 10 Seconds wait Completed\n");
+        }
+    }
+}
