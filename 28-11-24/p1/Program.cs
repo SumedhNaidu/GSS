3a6cf7065e4e83dde3f0398518bf3540e4422fce
SumedhNaidu <sumedhnaidu.kn@gmail.com>
2024-11-28 18:50:51 +0530

    28-11-24

---
+++ 28-11-24/p1/Program.cs
@@ -0,0 +1,102 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RealTimeExample_of_ConcurrentTasks
+{
+    internal class Program
+    {
+        static SemaphoreSlim semaphoreSlim = new SemaphoreSlim(3);
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            Console.WriteLine($"Main Thread Started");
+            List<CreditCard> creditCards = CreditCard.GenerateCreditCards(15);
+            Console.WriteLine($"Credit Card Generated : {creditCards.Count}");
+            ProcessCreditCards(creditCards);
+            Console.WriteLine($"Main Thread Completed");
+            Console.ReadKey();
+        }
+        public static async void ProcessCreditCards(List<CreditCard> creditCards)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var tasks = new List<Task<string>>();
+            tasks = creditCards.Select(async card =>
+            {
+                await semaphoreSlim.WaitAsync();
+                try
+                {
+                    return await ProcessCard(card);
+                }
+                finally
+                {
+                    semaphoreSlim.Release();
+                }
+            }).ToList();
+            var Responses = await Task.WhenAll(tasks);
+            List<CreditCardResponse> creditCardResponses = new List<CreditCardResponse>();
+            foreach (var response in Responses)
+            {
+                CreditCardResponse creditCardResponse = JsonConvert.DeserializeObject<CreditCardResponse>(response);
+                creditCardResponses.Add(creditCardResponse);
+            }
+            Console.WriteLine("\n Approved Credit Cards");
+            foreach(var item in creditCardResponses.Where(card => card.IsProcessed == true))
+            {
+                Console.WriteLine($"Card Number: {item.CardNumber}, Name: {item.Name}");
+            }
+            Console.WriteLine("\nRejected Credit Cards");
+            foreach (var item in creditCardResponses.Where(card => card.IsProcessed == false))
+            {
+                Console.WriteLine($"Card Number: {item.CardNumber}, Name: {item.Name}");
+            }
+        }
+        public static async Task<string>ProcessCard(CreditCard creditCard)
+        {
+            await Task.Delay(1000);
+            var creditCardResponse = new CreditCardResponse
+            {
+                CardNumber = creditCard.CardNumber,
+                Name = creditCard.Name,
+                IsProcessed = creditCard.CardNumber % 2 == 0 ? true : false
+
+            };
+            string jsonString = JsonConvert.SerializeObject(creditCardResponse);
+            return jsonString;
+        }
+    }
+    public class CreditCard
+    {
+        public long CardNumber { get; set; }
+        public string Name { get; set; }
+
+        public static List<CreditCard> GenerateCreditCards(int number)
+        {
+            List<CreditCard> creditCards = new List<CreditCard>();
+            for (int i = 0; i < number; i++)
+            {
+                CreditCard card = new CreditCard()
+                {
+                    CardNumber = 10000000 + i,
+                    Name = "CreditCard-" + i
+                };
+                creditCards.Add(card);
+            }
+            return creditCards;
+        }
+    }
+        public class CreditCardResponse
+        {
+            public long CardNumber { get; set; }
+            public string Name { get; set; }
+            public bool IsProcessed { get; set; }
+        }
+    }
+
---
+++ 28-11-24/p2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AsynchronousProgramming
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Main Method Started......");
+            Console.WriteLine("Enter the Name: ");
+            string Name = Console.ReadLine();
+
+            SomeMethod(Name);
+
+            Console.WriteLine("Main Method End");
+            Console.ReadKey();
+        }
+
+        public async static void SomeMethod(string Name)
+        {
+            Console.WriteLine("Some Method Started......");
+            var GreetingSMessage = await Greetings(Name);
+            Console.WriteLine($"\n{GreetingSMessage}");
+            Console.WriteLine("Some Method End");
+        }
+
+        public static async Task<string> Greetings(string Name)
+        {
+            string message = string.Empty;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:58937/");
+
+                HttpResponseMessage response = await client.GetAsync($"api/greetings/{Name}");
+                message = await response.Content.ReadAsStringAsync();
+            }
+            return message;
+        }
+    }
+}
