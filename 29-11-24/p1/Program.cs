de76801a4fd50325aee6fb49b9c072ac634b7d13
SumedhNaidu <sumedhnaidu.kn@gmail.com>
2024-11-29 18:46:16 +0530

    29-11-24

---
+++ 29-11-24/p1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Synchronization
+{
+    internal class Program
+    {
+        object lockObj = new object();
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Thread t1 = new Thread(p.SomeMethod)
+            {
+                Name = "Thread1"
+            };
+            Thread t2 = new Thread(p.SomeMethod)
+            {
+                Name = "Thread 2"
+            };
+            Thread t3 = new Thread(p.SomeMethod)
+            {
+                Name = "Thread 3"
+            };
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            Console.ReadKey();
+        }
+        void SomeMethod()
+        {
+
+            lock (lockObj)
+            {
+                Console.Write("Welcome to the ");
+                Thread.Sleep(1000);
+                Console.WriteLine("world of Dotnet");
+            }
+           
+        }
+    }
+}
---
+++ 29-11-24/p2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPoolPractice
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Thread t1 = new Thread(new ParameterizedThreadStart(MyMethod));
+            t1.Start(2);
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(MyMethod));
+            }
+            Console.ReadKey();
+        }
+        public static void MyMethod(object obj)
+        {
+            Thread t = Thread.CurrentThread;
+            string message = $"Background: {t.IsBackground}, Thread Pool:{t.IsThreadPoolThread}, Thread ID: {t.ManagedThreadId}";
+            Console.WriteLine(message);
+        }
+    }
+}
+
