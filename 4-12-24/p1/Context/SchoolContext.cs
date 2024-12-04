d5170886f39e146792eced0c8a02dcbd649d0a5e
SumedhNaidu <sumedhnaidu.kn@gmail.com>
2024-12-04 18:58:18 +0530

    4-12-24

---
+++ 4-12-24/p1/Context/SchoolContext.cs
@@ -0,0 +1,38 @@
+﻿using APIwithEFcore.DBModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIwithEFcore.Context
+{
+    public class SchoolContext : DbContext
+    {
+        IConfiguration Configuration { get; }
+
+        public DbSet<Student> Students {  get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Teacher> Teachers { get; set; }
+
+        public SchoolContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Fluent API
+            // configure the relationship between Student and Course
+            modelBuilder.Entity<Student>()
+                .HasOne<Course>(s => s.Course)
+                .WithMany(c => c.Students)
+                .HasForeignKey(s => s.CourseId);
+
+            // configure default values for the student
+            modelBuilder.Entity<Student>()
+                .Property(s => s.DateAdded)
+                .HasDefaultValue("getdate()");
+        }
+    }
+}
